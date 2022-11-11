d688630ba1a86059f9f68d0c88ad0d88b8f2860b
addy0110 <addycoder0110@gmail.com>
2022-11-11 17:12:22 +0530

    Added radio button in RegisterFragment.kt

--- app/src/main/java/com/example/foodonate/RegisterFragment.kt
+++ app/src/main/java/com/example/foodonate/RegisterFragment.kt
@@ -5,14 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.Toast
+import com.example.foodonate.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
+    lateinit var binding: FragmentRegisterBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        binding = FragmentRegisterBinding.inflate(layoutInflater,container,false)
+        return binding.root
     }
 
 }
